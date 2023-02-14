00f4d58ae969aab8800c72854288a61f501b3c74
waldropj4949 <125215758+waldropj4949@users.noreply.github.com>
2023-02-14 16:30:36 -0500

    Add files via upload
    File is homework 1 for CTI-110. This output demonstrates squared and cubed of any integer provided.

---
+++ P1HW1_WaldropJames.py
@@ -0,0 +1,9 @@
+user_num = int(input('Enter integer:\n'))
+print('You entered:', user_num)
+user_num_sq = user_num * user_num
+print(user_num, 'squared is', user_num_sq)
+user_num_cu = user_num * user_num * user_num
+print('And', user_num, 'cubed is', user_num_cu, '!!')
+user_num2 = int(input('Enter another integer:\n'))
+print('4 +', user_num2, 'is', user_num2 + 4)
+print('4 *', user_num2, 'is', user_num2 * 4)
